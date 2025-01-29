785d04c1b12270de42835a6a53615cf21558663c
alexpiedone <alexdobre229@yahoo.ro>
2025-01-29 11:33:21 +0200

    Add project files.

---
+++ Models.cs
@@ -0,0 +1,10 @@
+﻿
+public class Models
+{
+
+    public static readonly string _assetsPath = Path.Combine(Environment.CurrentDirectory, "assets");
+    public static readonly string _imagesFolder = Path.Combine(_assetsPath, "images");
+    public static readonly string _samplesFolder = Path.Combine(_imagesFolder, "samples");
+    public static readonly string _predictSingleImage = Path.Combine(_imagesFolder, "predict3.jpg");
+
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+namespace teste;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        //CoffeeBoxDetector.DetectTextWithOCR(Models._predictSingleImage, Path.Combine(Models._assetsPath, "output"));
+
+        CoffeeBoxDetector.DetectObjectsMatchingTemplates(Models._predictSingleImage);
+    }
+}
